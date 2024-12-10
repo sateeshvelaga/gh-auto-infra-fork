f5828aa117f68704241e55653e877e27a4ce0543
TEK Student <TEKstudent@vlab.lan>
2024-12-10 14:40:05 -0700

    Update S3 backend configuration

--- labs/gh-actions/main/backend.tf
+++ labs/gh-actions/main/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "my-terraform-state-bucket019179"
+    bucket = "my-terraform-state-bucket-sateeshvelaga"
     key    = "terraform.tfstate"
     region = "us-east-1"
   }
